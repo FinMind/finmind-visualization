b13c9cb3902c96b6e2af51a30789d937a0a613af
sam <samlin266118@gmail.com>
2022-07-18 01:27:27 +0800

    feat: remove not usage

--- genenv.py
+++ genenv.py
@@ -1,21 +1,15 @@
-import socket
 import os
 from configparser import ConfigParser
 
-
-HOME_PATH = "/".join(os.path.abspath(__file__).split("/")[:-1])
-HOST_NAME = socket.gethostname()
 
 local_config = ConfigParser()
 local_config.read("local.ini")
 if os.environ.get("VERSION", ""):
     section = local_config[os.environ.get("VERSION", "")]
-elif HOST_NAME in local_config:
-    section = local_config[HOST_NAME]
 else:
     section = local_config["DEFAULT"]
 
-env_content = "HOSTNAME={}\nHOME_PATH={}\n".format(HOST_NAME, HOME_PATH)
+env_content = ""
 for sec in section:
     env_content += "{}={}\n".format(sec.upper(), section[sec])
 
